60d3324b174bdf92df2b44dfda7370c666bebdd6
ojhapall <68453460+ojhapall@users.noreply.github.com>
2021-02-07 15:47:25 -0500

    update

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -15,18 +15,28 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
-let baseMaps = {
-    Street: streets,
-    Dark: dark
-  };
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [streets]
-})
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Street": streets,
+    "Satellite:": satelliteStreets,
+    "Dark": dark
+  };
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
